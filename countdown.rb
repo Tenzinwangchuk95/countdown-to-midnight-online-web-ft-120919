f02c6cdacd7ea27b1e7a5db6b5d38dc6564ab1aa
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-10 17:26:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 def countdown(number)
-  seconds=10
-  while seconds > 0 
-  puts " #{seconds} SECOND(S)!"
-  seconds-=1 
+  number=number
+  while number > 0 
+  puts " #{number} SECOND(S)!"
+  number-=1 
   end
 return "HAPPY NEW YEAR!"
 end
